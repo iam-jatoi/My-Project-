4014da04903336f3d3da5a1d0dd23f68be1bcdf2
Jabbar Jatoi <jabbar.jatoi99@gmail.com>
2025-02-22 22:39:26 +0500

    Mindset_Growth_Project.py

--- Mindset_Growth_Project.py
+++ Mindset_Growth_Project.py
@@ -1,7 +1,7 @@
 # streamlit
 import streamlit as st
 
-st.set_page_config(page_title="Growth Mindset Project By Jabbar Jatoi", project_icon="💿")
+st.set_page_config(page_title="Growth Mindset Project By Jabbar Jatoi", project_icon="🌟")
 st.title("Growth Mindset AI Project By Jabbar Jatoi")
 
 st.header("🚀 Welcome to Your Growth Journey!")
