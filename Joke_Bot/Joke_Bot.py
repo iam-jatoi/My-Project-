2bead17805aec893591a56636febbe348d79d208
Jabbar Jatoi <jabbar.jatoi99@gmail.com>
2025-03-22 20:15:08 +0500

    Joke_Bot.py

--- Joke_Bot/Joke_Bot.py
+++ Joke_Bot/Joke_Bot.py
@@ -1,1 +1,13 @@
 
+Joke = "Here is a joke for you! Panaversity GPT - Sophia is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Sophia returns with 13 liters of milk. The programmer asks why and Sophia replies: 'because they had eggs.'"
+Sorry = "I Only Tell Jokes"
+
+def bot():
+    user_input = input("What Do You Want? ")
+
+    if "Joke" in user_input:  # Fixed variable name
+        print(Joke)
+    else:
+        print("I Don't Understand")
+
+bot()
